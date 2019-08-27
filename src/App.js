decb192bfebf34705bc953ed65a56a6b6ccd0d59
Jon Baker <jon.david.baker@gmail.com>
2019-08-27 16:26:31 -0500

    deploying to heroku

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/styles.css';
+import Calculator from './layout/calculator/Calculator';
+
+
+
+
+  const app = () => (
+    <div className="app">
+      <Calculator />
+    </div>
+  );
+
+  
+
+export default app; 
+
